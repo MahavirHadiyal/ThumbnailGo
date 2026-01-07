229449e30c73814fc483067a957a6f69ec6c85cc
Hadiyal Mahavir <mghadiyal0@gmail.com>
2026-01-07 18:50:33 +0530

    origin change

--- server/server.ts
+++ server/server.ts
@@ -23,7 +23,7 @@ await connectDB()
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:3000','https://thumbnail-go-frontend.vercel.app',''],
     credentials: true
 }))
 
